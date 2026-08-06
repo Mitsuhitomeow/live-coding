499da92cd205802140255d21084fdda0149c4300
mitsuhitomeow <cancem343@gmail.com>
2026-08-06 10:07:32 +0200

    feat: add footer styles

--- language-app/src/App.ts
+++ language-app/src/App.ts
@@ -1,14 +1,16 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import BaseComponent from '@/components/BaseComponent';
+import styles from '@/App.module.scss';
 
 export default function App(): HTMLElement {
   return BaseComponent({
-    className: 'container',
+    className: styles.container,
     children: [
       Header(),
       BaseComponent({
         tag: 'main',
+        className: styles.main,
         attributes: {
           id: 'outlet',
         },
--- language-app/src/components/Footer/index.ts
+++ language-app/src/components/Footer/index.ts
@@ -1,13 +1,14 @@
 import BaseComponent from '@/components/BaseComponent';
+import styles from '@/components/Footer/footer.module.scss';
 
 export default function Footer(): HTMLElement {
   return BaseComponent({
     tag: 'footer',
-    className: 'footer',
+    className: styles.footer,
     children: [
       BaseComponent({
         tag: 'span',
-        className: 'footer-text',
+        className: styles.footerText,
         text: 'my vibe coding language app 2026',
       }),
     ],
